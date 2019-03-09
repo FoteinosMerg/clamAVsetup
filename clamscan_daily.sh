3ae79138977e20a67fc4607b61616041b2a4282b
Foteinos <foteinosmerg@gmail.com>
2019-03-09 20:38:19 +0100

    paramtrize

--- clamscan_daily.sh
+++ clamscan_daily.sh
@@ -3,7 +3,11 @@
 # USAGE
 #./clamscan_daily.sh <DIRTOSCAN> <EMAIL_TO>
 
-LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+sudo freshclam -v
+
+# sudo mkdir -p logs
+touch clamav-$(sudo date +'%Y-%m-%d').log
+LOGFILE="clamav-$(sudo date +'%Y-%m-%d').log" #"/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
 EMAIL_MSG="Please see the log file attached.";
 DIRTOSCAN=$1;
 EMAIL_TO=$2
@@ -39,11 +43,11 @@ MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
 # if the value is not equal to zero, send an email with the log file attached
 if [ "$MALWARE" -ne "0" ];then
   #using heirloom-mailx below
-  sudo echo "$EMAIL_MSG" | sudo mutt -s "Malware Found" "$EMAIL_TO" -a "$LOGFILE" #-s "Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO";
+  echo "$EMAIL_MSG" | sudo mutt -s "Malware Found" "$EMAIL_TO" -a "$LOGFILE" #-s "Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO";
 fi
 
-sudo echo "$EMAIL_MSG" | sudo mutt -s "Malware Found" "$EMAIL_TO" -a "$LOGFILE" #
+echo "$EMAIL_MSG" | sudo mutt -s "Malware Found" "$EMAIL_TO" -a "$LOGFILE" #
 
 exit 0
-
-# echo "_test attach" | mutt -s "_subject" foteinosmerg@protonmail.com -a .muttrc
+#
+# # echo "_test attach" | mutt -s "_subject" foteinosmerg@protonmail.com -a .muttrc
