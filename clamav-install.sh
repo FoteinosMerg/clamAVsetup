6ae27b2454f7773d886c041a20dee7ba94037b3b
Foteinos <foteinosmerg@gmail.com>
2019-03-10 14:00:40 +0100

    some refactoring and styling

--- clamav-install.sh
+++ clamav-install.sh
@@ -2,74 +2,78 @@
 
 echo
 
-# Find OS
+# Find OS ----------------------------------------------------------------------
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
     Darwin*)    machine=Mac;;
     # CYGWIN*)    machine=Cygwin;;
     # MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${unameOut}"
+    *)          machine="UNKNOWN: ${unameOut}"
 esac
 
-# Install separately some prerequisites ----------------------------------------
-
+# Start installation process ---------------------------------------------------
 
 if [ ${machine} = "Linux" ]; then
+
 	sudo apt-get update
 
-	packages=(gcc clang build-essential openssl libssl-dev libcurl4-openssl-dev zlib1g-dev libpng-dev libxml2-dev libjson-c-dev libbz2-dev libpcre3-dev ncurses-dev)
+  # Install separately Linux prerequisites -------------------------------------
+
+	packages=(
+      gcc
+      clang
+      build-essential
+      openssl
+      libssl-dev
+      libcurl4-openssl-dev
+      zlib1g-dev
+      libpng-dev
+      libxml2-dev
+      libjson-c-dev
+      libbz2-dev
+      libpcre3-dev
+      ncurses-dev
+  )
 	already_installed=()
 	for package in "${packages[@]}"
 	do
 	  dpkg -s $package >/dev/null 2>&1 && {
-	      echo "*** $package" already installed
+	      echo "* $package already installed"
 	  } || {
 	      sudo apt-get install -y $package
 	      already_installed+=( $package )
 	  }
 	done
 
-	# Installation of main packages ------------------------------------------------
+  # Installation of main packages ----------------------------------------------
 
-	sudo apt-get install clamav clamav-freshclam #heirloom-mailx
+	sudo apt-get install clamav clamav-freshclam
 
+  # freshclam configuration ----------------------------------------------------
 
+  sudo mkdir -p /etc/clamav
+  if [ ! -f /etc/clamav/freshclam.conf ]; then
+      sudo cp freshclam.conf /etc/clamav/freshclam.conf
+  fi
 
-# freshclam configuration ------------------------------------------------------
+  sudo service clamav-freshclam start # /etc/init.d/clamav-freshclam start
+  sudo systemctl daemon-reload
 
-sudo mkdir -p /etc/clamav
-if [ ! -f /etc/clamav/freshclam.conf ]; then
-    sudo cp freshclam.conf /etc/clamav/freshclam.conf
-fi
+  # Display info about freshclam update
+  ps -ef | grep fresh | grep clam
+  grep -i check /etc/clamav/freshclam.conf
 
-# /etc/init.d/clamav-freshclam start
-sudo service clamav-freshclam start
-sudo systemctl daemon-reload
+  sudo mkdir -p /var/lib/clamav # Database dir creation
+  sudo ldconfig # Download signature database
 
-# Display info about freshclam update (manual update with: freshclam -v)
-ps -ef | grep fresh | grep clam
-grep -i check /etc/clamav/freshclam.conf
+  # Users and user-privileges cnfiguration -------------------------------------
 
-
-# Databases creation -----------------------------------------------------------
-
-sudo mkdir -p /var/lib/clamav
-
-# Users and user-privileges cnfiguration ---------------------------------------
-
-# Delete group if it already exists
-sudo sed -i '/clamav/d' /etc/group
-# Create the clamav group
-sudo groupadd clamav
-# Create the clamav user account
-sudo useradd -g clamav -s /bin/false -c "clamAv" clamav
-# Set user ownership for the database directory
-sudo chown -R clamav:clamav /usr/local/share/clamav
-
-# Download and update signature databases --------------------------------------
-sudo ldconfig
-sudo freshclam
+  sudo sed -i '/clamav/d' /etc/group # Delete group if it already exists
+  sudo groupadd clamav # Create the clamav group
+  sudo useradd -g clamav -s /bin/false -c "clamAv" clamav # Create the clamav user account
+  sudo chown -R clamav:clamav /usr/local/share/clamav # Set user ownership for the database directory
 
 elif [ ${machine} = "Mac" ]; then
 	which -s brew
@@ -82,8 +86,8 @@ elif [ ${machine} = "Mac" ]; then
 	fi
 	brew reinstall -f clamav
 
+	# freshclam configuration ----------------------------------------------------
 
-	# freshclam configuration ------------------------------------------------------
 	# cd /usr/local/etc/clamav
 	# cp /usr/local/etc/clamav/freshclam.conf.sample /usr/local/etc/clamav/freshclam.conf
 	cp freshclam.conf /usr/local/etc/clamav/freshclam.conf
@@ -96,32 +100,39 @@ elif [ ${machine} = "Mac" ]; then
 	ps -ef | grep fresh | grep clam
 	grep -i check  /usr/local/etc/clamav/freshclam.conf
 
-	# Databases creation -----------------------------------------------------------
+	# Database creation ----------------------------------------------------------
 
 	sudo mkdir -p /var/lib/clamav
 	sudo chown -R clamav:wheel /var/log/clamav/
 
-	# Users and user-privileges cnfiguration ---------------------------------------
+  sudo update_dyld_shared_cache # Download signature database
 
-	# Delete group if it already exists
-	sudo dscl . -create /Groups/clamav
-	# Create the clamav user account
-	sudo dscl . -create /Users/clamav
-	# Set user ownership for the database directory
-	# bug: group cannot be assigned
-	sudo chown -R clamav: /usr/local/share/clamav
+	# Users and user-privileges cnfiguration -------------------------------------
 
-	# Download and update signature databases --------------------------------------
-	sudo update_dyld_shared_cache
-	sudo freshclam
+	sudo dscl . -create /Groups/clamav # Delete group if it already exists
+	sudo dscl . -create /Users/clamav # Create the clamav user account
+  # bug: group cannot be assigned
+	sudo chown -R clamav: /usr/local/share/clamav # Set user ownership for the db dir
+
+else
+  echo "OS unknown. Exiting"
+  exit 2
 fi
 
+# Update database (final check) ------------------------------------------------
+
+echo "-------------------------------------------------------------------------"
+echo "DATABASE UPDATE"
+echo ""
+sudo freshclam --verbose
 
 sleep .5
 echo "-------------------------------------------------------------------------"
+clamscan -V
+echo ""
 which clamscan
 which freshclam
 echo "-------------------------------------------------------------------------"
 sleep .5
 
-
+exit 0
