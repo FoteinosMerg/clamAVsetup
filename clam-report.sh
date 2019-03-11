767f3277646a3a1ccb7528d9c98147cf5eb7b426
Foteinos <foteinosmerg@gmail.com>
2019-03-11 10:39:43 +0100

    styling

--- clam-report.sh
+++ clam-report.sh
@@ -2,10 +2,13 @@
 
 # USAGE
 #./clam-report.sh <DIR_TO_SCAN> <EMAIL_TO>
+#
 
+# Variables
 DIR_TO_SCAN=$1;
 DIR_SIZE=$(sudo du -sh "$DIR_TO_SCAN" 2>/dev/null | cut -f1);       # humam readable size
 DIR_SIZE_KBYTES=$(sudo du -s "$DIR_TO_SCAN" 2>/dev/null | cut -f1); # size in kilobytes
+N=0                                                                 # Niceness (default value)
 EMAIL_TO=$2
 EMAIL_BODY="Please read the .log file attached"
 SUBJECT="* ClamAV: MALWARE FOUND *"
@@ -25,7 +28,6 @@ echo "ClamAV database updated"
 sleep .75
 
 # Resolve niceness: rude to others (bigger size -> higher priority)
-N=0
 if [ "$DIR_SIZE_KBYTES" -ge $((5*$((10**8)))) ]; then  # 500 GB <= size
   N=-10
 elif [ "$DIR_SIZE_KBYTES" -ge $((10**8)) ]; then       # 100 GB <= size < 500 GB
@@ -39,6 +41,7 @@ elif [ "$DIR_SIZE_KBYTES" -ge $((10**5)) ]; then       # 100 MB <= size < 1 GB
 fi
 
 # Print info about scanning to follow
+
 echo
 echo "Directory to be scanned    : "$DIR_TO_SCAN""
 sleep .75
@@ -51,22 +54,22 @@ sleep .75
 
 LOG_FILE="clamav_$(sudo date +"%Y-%m-%d_%H-%M-%S").log" # Identified by date-time
 touch "$LOG_FILE"
+
 rm -rf INFECTED/* && mkdir -p INFECTED
 
-# Start scanning
+# Perform scanning
+
 echo "Started at                 : $(sudo date +"%H:%M:%S, %Y-%m-%d")"
 echo
 echo "Scanning..."
 echo
 sudo nice -n"$N" clamscan -ri --move=INFECTED --exclude-dir=./INFECTED/ "$DIR_TO_SCAN" &>"$LOG_FILE"
-# sudo clamscan -ri "$DIR_TO_SCAN" &>"$LOG_FILE"
-# sudo nice -n5 clamscan -ri / --exclude-dir=/sys/ &>"$LOG_FILE";
 
-# Create and send report mail
+# Create and send report mail, if infected files have been detected
 
 MALWARE=$(tail "$LOG_FILE" | grep Infected | cut -d" " -f3);
 
-if [ "$MALWARE" -ne "0" ]; then              # Infected files have been detected
+if [ "$MALWARE" -ne "0" ]; then           # Infected files have been detected
 
   echo "Infected files detected. Sending report..."
   tar -czf INFECTED.tar.gz INFECTED
@@ -76,16 +79,14 @@ if [ "$MALWARE" -ne "0" ]; then              # Infected files have been detected
   echo
   echo "Report has been sent"
 
-  rm INFECTED.tar.gz
+  rm INFECTED.tar.gz                      # No infected files have been detected
 
 else
 
   echo "No infected files have been detected"
-
 fi
 
-# Delete log-file and directory of infected files
-rm -r "$LOG_FILE" INFECTED
-
+rm -r "$LOG_FILE" INFECTED # Delete log-file and directory of infected files
 echo
+echo "Script finished."
 exit 0
