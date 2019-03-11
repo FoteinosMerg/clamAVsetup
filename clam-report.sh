7db946aa43e4c60cb948e20ac570a5106e03587e
Foteinos <foteinosmerg@gmail.com>
2019-03-11 10:45:27 +0100

    further styling

--- clam-report.sh
+++ clam-report.sh
@@ -8,7 +8,7 @@
 DIR_TO_SCAN=$1;
 DIR_SIZE=$(sudo du -sh "$DIR_TO_SCAN" 2>/dev/null | cut -f1);       # humam readable size
 DIR_SIZE_KBYTES=$(sudo du -s "$DIR_TO_SCAN" 2>/dev/null | cut -f1); # size in kilobytes
-N=0                                                                 # Niceness (default value)
+N=0                                                                 # niceness (default value)
 EMAIL_TO=$2
 EMAIL_BODY="Please read the .log file attached"
 SUBJECT="* ClamAV: MALWARE FOUND *"
@@ -28,6 +28,7 @@ echo "ClamAV database updated"
 sleep .75
 
 # Resolve niceness: rude to others (bigger size -> higher priority)
+
 if [ "$DIR_SIZE_KBYTES" -ge $((5*$((10**8)))) ]; then  # 500 GB <= size
   N=-10
 elif [ "$DIR_SIZE_KBYTES" -ge $((10**8)) ]; then       # 100 GB <= size < 500 GB
@@ -50,7 +51,7 @@ sleep .75
 echo "Niceness level             : $N"
 sleep .75
 
-# Create log-file and directory for collected files
+# Create log-file and directory of detected files
 
 LOG_FILE="clamav_$(sudo date +"%Y-%m-%d_%H-%M-%S").log" # Identified by date-time
 touch "$LOG_FILE"
@@ -67,9 +68,9 @@ sudo nice -n"$N" clamscan -ri --move=INFECTED --exclude-dir=./INFECTED/ "$DIR_TO
 
 # Create and send report mail, if infected files have been detected
 
-MALWARE=$(tail "$LOG_FILE" | grep Infected | cut -d" " -f3);
+MALWARE=$(tail "$LOG_FILE" | grep Infected | cut -d" " -f3); # Number of infected files
 
-if [ "$MALWARE" -ne "0" ]; then           # Infected files have been detected
+if [ "$MALWARE" -ne "0" ]; then                              # Infected files have been detected
 
   echo "Infected files detected. Sending report..."
   tar -czf INFECTED.tar.gz INFECTED
@@ -79,14 +80,15 @@ if [ "$MALWARE" -ne "0" ]; then           # Infected files have been detected
   echo
   echo "Report has been sent"
 
-  rm INFECTED.tar.gz                      # No infected files have been detected
+  rm INFECTED.tar.gz
 
-else
+else                                                         # No infected files have been detected
 
   echo "No infected files have been detected"
+
 fi
 
-rm -r "$LOG_FILE" INFECTED # Delete log-file and directory of infected files
+rm -r "$LOG_FILE" INFECTED # Delete log-file and directory of detected files
 echo
 echo "Script finished."
 exit 0
